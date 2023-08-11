961f7aebed3919101ee066c2845ab55f1d01e3ad
Charles Vonness <cvonness@gmail.com>
2023-08-10 21:20:25 -0400

    More refactoring.

--- src/test/kotlin/TestSuite.kt
+++ src/test/kotlin/TestSuite.kt
@@ -2,25 +2,29 @@ import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 
-class TestSuite {
+object TestSuite {
+    private const val DAY1_PART1_EXPECTED_TOTAL = 69281
+    private const val DAY1_PART2_EXPECTED_TOTAL = 201524
+    private const val DAY2_PART1_EXPECTED_SCORE = 11841
+    private const val DAY2_PART2_EXPECTED_SCORE = 13022
 
     @Test
     fun testDay1Part1() {
-        assertEquals(69281, Day1Part1.calculateTotal().value)
+        assertEquals(DAY1_PART1_EXPECTED_TOTAL, Day1Part1.calculateTotal().value)
     }
 
     @Test
     fun testDay1Part2() {
-        assertEquals(201524, Day1Part2.calculateTotal())
+        assertEquals(DAY1_PART2_EXPECTED_TOTAL, Day1Part2.calculateTotal())
     }
 
     @Test
     fun testDay2Part1() {
-        assertEquals(11841, Day2Part1.calculateScore())
+        assertEquals(DAY2_PART1_EXPECTED_SCORE, Day2Part1.calculateScore())
     }
 
     @Test
     fun testDay2Part2() {
-        assertEquals(13022, Day2Part2.calculateScore())
+        assertEquals(DAY2_PART2_EXPECTED_SCORE, Day2Part2.calculateScore())
     }
 }
