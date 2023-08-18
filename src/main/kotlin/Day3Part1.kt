db1ca03c02213125323d2e3a4b72394ba73606b2
Charles Vonness <cvonness@gmail.com>
2023-08-17 21:11:46 -0400

    Switched to use fold.

--- src/main/kotlin/Day3Part1.kt
+++ src/main/kotlin/Day3Part1.kt
@@ -7,19 +7,17 @@ object Day3Part1 {
     fun calculatePriority(char: Char): Int =
         if (char.isLowerCase()) char.code - 96 else char.code - 64 + 26
 
-    fun calculateSum() =
+    fun calculateSum(): Int =
         File(FILE)
             .readLines()
-            .map {
-                val charList = it.toCharArray().toList()
-                val halves = charList.chunked(charList.size / 2)
-                halves[0].intersect(halves[1])
-            }
+            .map { it.toCharArray().toList().chunked(it.length / 2) }
+            .map { it.fold(it.first()) { acc, list -> acc.intersect(list.toSet()).toList() } }
             .flatten()
             .sumOf { calculatePriority(it) }
 
+
     @JvmStatic
     fun main(args: Array<String>) {
-        calculateSum().also { println(it) }
+        println(calculateSum())
     }
 }
