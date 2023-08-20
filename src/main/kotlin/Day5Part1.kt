5329acd6d8304e4ab0c84ee855d58c3e4536987e
Charles Vonness <cvonness@gmail.com>
2023-08-20 16:38:31 -0400

    Lots of refactoring.

--- src/main/kotlin/Day5Part1.kt
+++ src/main/kotlin/Day5Part1.kt
@@ -7,69 +7,71 @@ object Day5Part1 {
 
     const val FILE = "src/main/resources/Day5Part1Input.txt"
 
-    @JvmStatic
-    fun main(args: Array<String>) {
-        println(calculateResult())
+    data class Instruction(val quantity: Int, val from: Int, val to: Int) {
+        private fun move(stacks: List<Stack<Char>>) = stacks[to].push(stacks[from].pop())
+
+        private fun skipEmpties(stacks: List<Stack<Char>>) {
+            while (stacks[from].peek() == ' ') stacks[from].pop()
+        }
+
+        fun execute(stacks: List<Stack<Char>>) =
+            repeat(quantity) {
+                skipEmpties(stacks)
+                move(stacks)
+            }
     }
 
-    fun calculateResult(): String {
-        val startingState =
-            File(FILE)
-                .readLines()
-                .takeWhile { it.isNotEmpty() }
-                .dropLast(1)
-                .map { it.toCharArray().toList() }
+    private fun processRow(stacks: MutableList<Stack<Char>>, row: List<Char>) =
+        repeat(NUM_COLUMNS) { columnNumber ->
+            stacks[columnNumber].push(
+                row
+                    .drop(columnNumber * COLUMN_WIDTH)
+                    .take(COLUMN_WIDTH)
+                    .joinToString("")
+                    .trim()
+                    .replace("[", "")
+                    .replace("]", "")
+                    .firstOrNull() ?: ' '
+            )
+        }
 
-        val stacks = createStacks(startingState)
+    private fun createStacks(): List<Stack<Char>> =
+        mutableListOf<Stack<Char>>()
+            .apply { repeat(NUM_COLUMNS) { add(Stack()) } }
+            .also { stacks ->
+                File(FILE)
+                    .readLines()
+                    .takeWhile { it.isNotEmpty() }
+                    .dropLast(1)
+                    .map { it.toCharArray().toList() }
+                    .reversed()
+                    .forEach { processRow(stacks, it) }
+            }
 
-        val instructions =
-            File(FILE)
-                .readLines()
-                .dropWhile { it.isNotEmpty() }
-                .drop(1)
-
-        instructions.forEach { instruction ->
-            instruction.replace("move ", "").replace(" from ", ",").replace(" to ", ",").split(",").let {
-                val quantity = it[0].toInt()
-                val from = it[1].toInt()
-                val to = it[2].toInt()
-                repeat(quantity) {
-                    while (stacks[from - 1].peek() == ' ') {
-                        stacks[from - 1].pop()
+    private fun processInstructions(stacks: List<Stack<Char>>): List<Stack<Char>> =
+        File(FILE)
+            .readLines()
+            .dropWhile { it.isNotEmpty() }
+            .drop(1)
+            .forEach { instructionStr ->
+                instructionStr
+                    .replace("move ", "")
+                    .replace(" from ", ",")
+                    .replace(" to ", ",")
+                    .split(",")
+                    .let {
+                        Instruction(
+                            quantity = it[0].toInt(),
+                            from = it[1].toInt() - 1,
+                            to = it[2].toInt() - 1
+                        ).execute(stacks)
                     }
-                    stacks[to - 1].push(stacks[from - 1].pop())
-                }
-            }
-        }
+            }.let { stacks }
 
-        val result = StringBuilder()
+    private fun getTopOfStack(stack: Stack<Char>) = if (stack.isEmpty()) " " else stack.peek()
 
-        stacks.forEach { stack -> if (stack.isEmpty()) result.append(" ") else result.append(stack.peek()) }
+    fun calculateResult(): String = processInstructions(createStacks()).map(::getTopOfStack).joinToString("")
 
-        return result.toString()
-    }
-
-    private fun createStacks(startingState: List<List<Char>>): List<Stack<Char>> {
-        val stacks = mutableListOf<Stack<Char>>()
-
-        repeat(NUM_COLUMNS) {
-            stacks.add(Stack())
-        }
-
-        startingState.reversed().forEachIndexed { rowNumber: Int, row: List<Char> ->
-            repeat(NUM_COLUMNS) { columnNumber ->
-                val value =
-                    row
-                        .drop(columnNumber * COLUMN_WIDTH)
-                        .take(COLUMN_WIDTH)
-                        .joinToString("")
-                        .trim()
-                        .replace("[", "")
-                        .replace("]", "")
-                stacks[columnNumber].push(value.firstOrNull() ?: ' ')
-            }
-        }
-
-        return stacks
-    }
+    @JvmStatic
+    fun main(args: Array<String>) = println(calculateResult())
 }
